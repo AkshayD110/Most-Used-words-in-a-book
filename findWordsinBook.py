f222f593b0d7be984a7f8a82eafe1bf0806045a7
AkshayD110 <akshayd110@gmail.com>
2018-02-25 03:36:58 +0530

    initial commit

--- findWordsinBook.py
+++ findWordsinBook.py
@@ -1,4 +1,3 @@
-import numpy
 import collections
 import PyPDF2
 import re
@@ -10,7 +9,8 @@ def wordsFromPDFfile(path):
     pdfreader = PyPDF2.PdfFileReader(pdffileObj)
     numOfPages = pdfreader.getNumPages()
     pages = 1
-    print("Pages:",numOfPages)
+    print("==========processing==========")
+    #print("Pages:",numOfPages)
     """try using numpy for list operations below"""
     for pages in range(pages,numOfPages):
         pdfObject = pdfreader.getPage(pages)
@@ -18,20 +18,20 @@ def wordsFromPDFfile(path):
         for words in wordsInPage:
             listOfWords.append(words.lower())
         totalWord += int(len(wordsInPage))
-    #print(pdfObject.extractText())
-    #numpyArray = numpy.array(listOfWords)
-    #words = re.findall(r'\w+', pdfObject.extractText())
-    print("words:", totalWord)
-    print(collections.Counter(listOfWords).most_common(10))
+    #print("words:", totalWord)
+    commanWords=(collections.Counter(listOfWords).most_common(10))
+    print(f"The book has - \n {numOfPages} : no. of pages, \n {totalWord} : no. of words, \n The most common words in "
+          f"the book are : \n {commanWords}")
 
 def wordsFromTextFile(path):
     words = re.findall(r'\w+', open(path).read().lower())
-    print(type(words))
     print("words",len(words))
     print(collections.Counter(words).most_common(10))
 
 
 wordsFromPDFfile(r"C:\Users\akshdesh.ORADEV\Documents\books\python\fluent-python-2015-.pdf")
 #wordsFromTextFile(r"C:\Users\akshdesh.ORADEV\Documents\books\python\newFile.txt")
-wordsFromTextFile(r"D:\findWords.txt")
+#wordsFromTextFile(r"D:\findWords.txt")
 
+"Improvements -" \
+"> find the percentage of the occurance of the comman word"
